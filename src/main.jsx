e0a9c058df288981a9131112ba517b49907b9c4e
Aniket Chauhan <chauhananiket12801@gmail.com>
2023-06-26 11:32:38 +0530

    push project to github.

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+
+import { apiSlice } from "./api/apiSlice.js";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
+  </React.StrictMode>
+);
